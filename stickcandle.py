09dff1dc4376821d23748ef2328f25aeec1ce158
evanxu05@qq.com <evanxu05@qq.com>
2019-11-25 21:31:48 +0800

    4

--- stickcandle.py
+++ stickcandle.py
@@ -12,25 +12,24 @@ import mpl_finance as mpf
 from datetime import datetime
 import pandas as pd
 
-from WindPy import *
-w.start()
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+font = FontProperties(fname='/System/Library/Fonts/STHeiti Light.ttc')
 
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
 
-df_stockload=w.wsd("002283.SZ", "open,high,low,close,volume", "2019-1-1", "2019-11-25")
-df_stockload=pd.DataFrame(df_stockload.Data,index=df_stockload.Fields,columns=df_stockload.Times).T
+
+df_stockload = ts.pro_bar(ts_code='600580.SH',start_date='20191025', end_date='20191122', freq="5min",ma=[5, 10, 24,48,120,240])
+df_stockload = df_stockload.sort_index(ascending=False)
 
 fig = plt.figure(figsize=(8,6),dpi=100,facecolor='white')
 fig.subplots_adjust(left=0.09,bottom=0.20, right=0.94,top=0.90, wspace=0.2, hspace=0)
 graph_KAV = fig.add_subplot(1,1,1)
-mpf.candlestick2_ochl(graph_KAV, df_stockload.OPEN,df_stockload.CLOSE,df_stockload.HIGH,df_stockload.LOW, width=0.5, colorup='r', colordown='g')
-graph_KAV.set_title(u"002283 天润曲轴-日K线")
-graph_KAV.set_xlabel(u"日期")
-graph_KAV.set_ylabel(u"价格")
+mpf.candlestick2_ochl(graph_KAV, df_stockload.open,df_stockload.close,df_stockload.high,df_stockload.low, width=0.5, colorup='r', colordown='g')
+graph_KAV.set_title("600580 卧龙电驱-5M线")
+graph_KAV.set_xlabel("日期")
+graph_KAV.set_ylabel("价格")
 graph_KAV.set_xlim(0,len(df_stockload.index)) #设置一下x轴的范围
-graph_KAV.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定，每15天标一个日期
-graph_KAV.grid(True,color='k')
+graph_KAV.set_xticks(range(0,len(df_stockload.index),45))#X轴刻度设定，每15天标一个日期
+
 graph_KAV.set_xticklabels([df_stockload.index.strftime('%Y-%m-%d')[index] for index in graph_KAV.get_xticks()])#标签设置为日期
 
 #X轴每个ticker标签都向右倾斜45度 
