3a7bece0c11f25344b8abfeabf68c8ffa082b675
evanxu05 <evanxu05@qq.com>
2019-11-30 19:40:12 +0800

    git commit -m '20191130'

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 23 13:42:29 2019
+import pandas as pd 
 
-@author: xuyuanwu
-"""
-import pandas_datareader.data as web
-from datetime import datetime
-start=datetime(year=2017,
-               month=1,
-               day=1)
+otu = pd.read_excel(r'/Users/xuyuanwu/Desktop/Python/test/test.xlsx')
 
-today= datetime.now()
-
-df_stockload = web.DataReader('000001.SS','yahoo',start,today)
-
-df_stockload.Close.plot(c='b')
-df_stockload.Close.rolling(window=30).mean().plot(c='g')
-df_stockload.Close.rolling(window=60).mean().plot(c='r')
-plot.show()
+for index,row in otu.iterrows():
+    print(index)
+    print(row)
