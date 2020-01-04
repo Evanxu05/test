990fe4f198660ccbd7f5a606750ab78c770e24ff
Evanxu05 <evanxu05@qq.com>
2020-01-04 17:26:34 +0800

    git commit -m '20200104'

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-import pandas as pd 
-
-otu = pd.read_excel(r'/Users/xuyuanwu/Desktop/Python/test/test.xlsx')
-
-for index,row in otu.iterrows():
-    print(index)
-    print(row)
+print('hello world')
